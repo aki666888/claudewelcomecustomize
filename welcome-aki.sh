38a246e682e9508cf134ce2efce295aa7a162a21
aki666888 <aki.g.office@gmail.com>
2025-07-17 16:38:30 -0700

    Add files via upload

---
+++ welcome-aki.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Custom welcome script for Aki
+
+echo ""
+echo "╭────────────────────────────────────╮"
+echo "        ✨ Welcome To EVA 1.1 (beta)! ✨         
+echo "                                    
+echo "   Your Digital Pharmacy assistant is ready!      
+echo "                                    
+echo "╰────────────────────────────────────╯"
+echo ""
